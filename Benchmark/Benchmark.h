bd55901142685727de23012f2421a431be38622c
DAYNLIGHT <danielstodulski000@gmail.com>
2025-02-10 00:03:23 +0100

    allocs

--- Benchmark/Benchmark.h
+++ Benchmark/Benchmark.h
@@ -3,28 +3,63 @@
 #include <string>
 #include <vector>
 
+
 namespace Benchmark{
 class Benchmark{
-public:
+  public:
+  bool count_allocs = false;
+  size_t allocs;
+  size_t deallocs;
+  public:
   std::vector<std::string> result;
-public:
+  public:
   static Benchmark &get() {   
     static Benchmark benchmark;
     return benchmark; 
   };
 }; // class Benchmark
 
-struct Timer{
-private:
+class Timer{
+  private:
   const std::string label;
   std::chrono::time_point<std::chrono::high_resolution_clock> start;
-public:
-Timer(const std::string &label = "Timer") : label(label), start(std::chrono::high_resolution_clock::now()) {};
-~Timer(){
-  auto end = std::chrono::high_resolution_clock::now();
-  auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
-  std::string result = label + ": " + std::to_string(duration.count()) + " microseconds";
-  Benchmark::get().result.emplace_back(result);
-};
+  public:
+  Timer(const std::string &label = "Timer: ") : label(label), start(std::chrono::high_resolution_clock::now()) {};
+  ~Timer(){
+    auto end = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
+    Benchmark::get().result.emplace_back(label + ": " + std::to_string(duration.count()) + " microseconds");
+  };
 }; // struct Timer
-} // namespace Benchmark
+
+class Allocs{
+  private:
+    std::string label;
+  public:
+  Allocs(std::string label = "Allocs: ") : label(label)
+    { Benchmark::get().count_allocs = true; Benchmark::get().allocs = 0; Benchmark::get().deallocs = 0; };
+  ~Allocs() { 
+    Benchmark::get().count_allocs = false; 
+    Benchmark::get().result.emplace_back(label + ": " + std::to_string(Benchmark::get().allocs) + " allocs, " + std::to_string(Benchmark::get().deallocs) + " deallocs"); 
+  };
+}; // struct Allocs
+
+}; // namespace Benchmark
+
+void* operator new(size_t size) noexcept {
+  if(Benchmark::Benchmark::get().count_allocs)
+    Benchmark::Benchmark::get().allocs++;
+  return malloc(size);
+};
+
+void operator delete(void *ptr) noexcept{
+  if(Benchmark::Benchmark::get().count_allocs)
+    Benchmark::Benchmark::get().deallocs++;
+  free(ptr);
+};
+
+void operator delete(void *ptr, std::size_t size) noexcept {
+  if(Benchmark::Benchmark::get().count_allocs)
+      Benchmark::Benchmark::get().deallocs++;
+  free(ptr);
+}
--- Benchmark/main.cpp
+++ Benchmark/main.cpp
@@ -1,13 +1,30 @@
 #include "Benchmark.h"
+#include <vector>
+
+struct Vec3{
+  float x, y, z;
+  Vec3(float x, float y, float z) : x(x), y(y), z(z) {};
+};
 
 int main(){
 
   {
-    Benchmark::Timer timer("Print ! 10000 times");
+    std::vector<Vec3*> vec;
+
+    Benchmark::Timer timer("create vec3 10000 times");
+    Benchmark::Allocs allocs("allocs vec3 10000 times");
+
     for(int i = 0; i < 10000; i++)
-      printf("!");
+      vec.emplace_back(new Vec3(1.0f, 2.0f, 3.0f));
+
+    while(vec.size()) {
+      Vec3* vecN = vec.back();
+      vec.pop_back();
+      delete vecN;
+    };
   }
   
+  printf("\n");
   for(const std::string &line : Benchmark::Benchmark::get().result)
     printf("%s\n", line.c_str());
   return 0;
